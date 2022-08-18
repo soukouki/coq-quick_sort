a39368175832922dd25f050cc4f54560dc9ccc12
soukouki <soukouki0@yahoo.co.jp>
2022-08-18 23:24:33 +0900

    Sortedから新しい定義に書き直す!

--- sort.v
+++ sort.v
@@ -16,7 +16,41 @@ Local Open Scope list_scope.
 
 Lemma length_nil {A: Type}:
   length ([]: list A) = 0.
-Proof. by rewrite /length. Qed.
+Proof. by []. Qed.
+
+Lemma length_cons {A: Type} (xs: list A) (x: A):
+  length (x :: xs) = S (length xs).
+Proof. by []. Qed.
+
+Lemma sorted_min (xs: list nat) (x: nat):
+  Sorted (x :: xs) ->
+  forall y i: nat,
+  nth_error (x :: xs) i = Some y ->
+  x <= y.
+Proof.
+
+
+
+
+
+(* 
+Lemma nth_error_some_length {A: Type} (xs: list A) (i: nat):
+  (exists (x: A), nth_error xs i = Some x) -> i < length xs.
+Proof.
+induction xs.
+- case.
+  suff: nth_error ([]: list A) i = None.
+  + move=> Hnth_none.
+    by rewrite Hnth_none.
+  + apply nth_error_None.
+    rewrite length_nil.
+    by apply le_0_n.
+- case.
+  move=> x Hxs_some.
+  rewrite length_cons.
+(* ぜーんぜん解けないんだけど！？！？ *)
+Abort.
+ *)
 
 Lemma sorted_nil:
   Sorted [].
@@ -33,12 +67,36 @@ suff: nth_error ([]: list nat) i = None.
   by apply le_0_n.
 Qed.
 
+(* Lemma sorted_ind (x: nat) (xs: list nat):
+  Sorted xs -> (forall y, In y xs -> x <= y) -> Sorted (x :: xs).
+Proof.
+move=> Hxs_sorted Hy_in_xs.
+rewrite /Sorted.
+move=> x0 y0 i j Hx0 Hy0 Hi_lt_j. *)
+
+
+
+
+Lemma sorted_one(x: nat):
+  Sorted [x].
+Proof.
+rewrite /Sorted.
+move=> x0 y0 i j Hx0 Hy0.
+have: i = 0.
+Search (nth_error _ _= Some _) In.
+move: Hx0.
+rewrite nth_error_In.
+
+
+
 Lemma sorted_ind {A: Type} (xs: list nat) (x: nat):
   Sorted xs -> (forall y, In y xs -> x <= y) -> Sorted (x :: xs).
 Proof.
-move=> H_xs_sorted H_y_in_xs.
-rewrite /Sorted.
-move=> x0 y0 i j Hx0 Hy0 H_i_lt_j.
+induction xs.
+move=> _ _.
+
+
+
 
 
 
