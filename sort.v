1b271911a6bd5213a95b90a078b90bfd4cca938d
soukouki <soukouki0@yahoo.co.jp>
2022-08-18 15:28:53 +0900

    nth_errorつらくね？

--- sort.v
+++ sort.v
@@ -1,32 +1,45 @@
-Require Import Uint63 PArray.
+Require Import List PeanoNat.
+Import ListNotations.
 From mathcomp Require Import ssreflect.
+
+Set Implicit Arguments.
 
 Module Sort.
 
-Local Open Scope uint63_scope.
+Definition Sorted (xs: list nat): Prop :=
+  forall x y i j: nat,
+  nth_error xs i = Some x ->
+  nth_error xs j = Some y ->
+  i < j -> x <= y.
 
-Definition Sorted (arr: array nat) : Prop :=
-  forall x y: nat, forall i j: int,
-  x = arr.[i] ->
-  y = arr.[j] ->
-  is_true(i <=? j) ->
-    x <= y.
+Local Open Scope list_scope.
 
-Example sorted_5_5_5:
-  Sorted (make 3 5%nat).
+Lemma length_nil {A: Type}:
+  length ([]: list A) = 0.
+Proof. by rewrite /length. Qed.
+
+Lemma sorted_nil:
+  Sorted [].
 Proof.
 rewrite /Sorted.
-move=> x y i j Hx Hy H_i_leq_j.
-rewrite Hx Hy.
-by rewrite get_make get_make.
+move=> x y i j Hx Hy H_i_lt_j.
+move: Hx.
+suff: nth_error ([]: list nat) i = None.
+- move=> Hnth_none.
+  rewrite Hnth_none.
+  by [].
+- apply nth_error_None.
+  rewrite length_nil.
+  by apply le_0_n.
 Qed.
 
-Example sorted_1_2_4:
-  Sorted (make 3 0%nat)
-    .[0 <- 1%nat]
-    .[1 <- 2%nat]
-    .[2 <- 4%nat].
+Lemma sorted_ind {A: Type} (xs: list nat) (x: nat):
+  Sorted xs -> (forall y, In y xs -> x <= y) -> Sorted (x :: xs).
 Proof.
+move=> H_xs_sorted H_y_in_xs.
 rewrite /Sorted.
+move=> x0 y0 i j Hx0 Hy0 H_i_lt_j.
 
-これやめたほうが良い気がする
+
+
+
