16b7005a5c886e79d3cd42536ea18c0a924d9060
soukouki <soukouki0@yahoo.co.jp>
2022-08-21 01:51:27 +0900

    Propに置き換えたら、あの3日間の証明が全てby [].で終わることが判明してガン萎え

--- sort.v
+++ sort.v
@@ -9,90 +9,42 @@ Module Sort.
 Local Open Scope list_scope.
 Local Open Scope bool_scope.
 
-Fixpoint sorted (xs: list nat): bool :=
+Fixpoint sorted (xs: list nat): Prop :=
   match xs with
-  | [] => true
-  | x1 :: [] => true
-  | x1 :: (x2 :: _) as xs' => (x1 <=? x2) && sorted xs'
+  | [] => True
+  | x1 :: [] => True
+  | x1 :: (x2 :: _) as xs' => x1 <= x2 /\ sorted xs'
   end.
 
-(* (x1 :: x2 :: x3 :: x4 :: _) as xs *)
 Lemma sorted_ind: forall x1 x2 xs3,
-  sorted (x2 :: xs3) = true ->
+  sorted (x2 :: xs3) ->
   x1 <= x2 ->
-  sorted (x1 :: x2 :: xs3) = true.
+  sorted (x1 :: x2 :: xs3).
 Proof.
-move=> x1 x2 xs3.
-move: x1 x2.
-move: xs3.
-induction xs3.
-- move=> x1 x2 Hsorted Hx1_le_x2.
-  rewrite /sorted Bool.andb_true_iff.
-  split.
-  + by rewrite Nat.leb_le.
-  + by [].
-- move=> x1 x2 Hsorted Hx1_le_x2.
-  rename a into x3.
-  rewrite /sorted.
-  rewrite 2!Bool.andb_true_iff 2!Nat.leb_le.
-  split.
-    by [].
-  split.
-    move: Hsorted.
-    rewrite /sorted.
-    rewrite Bool.andb_true_iff Nat.leb_le.
-    by apply proj1.
-  rewrite -/sorted.
-  suff: sorted xs3 = true.
-    move=> Hxs3_sorted.
-    rewrite Hxs3_sorted.
-    case_eq xs3 => //=.
-    move=> x4 xs4 Hxs4.
-    rewrite Bool.andb_true_iff.
-    split.
-    + move: Hsorted.
-      rewrite /sorted Hxs4 2!Bool.andb_true_iff.
-      case. move=> _.
-      case. move=> H _.
-      by [].
-    + by [].
-  move: Hsorted.
-  rewrite {1}/sorted Bool.andb_true_iff.
-  case.
-  move=> Hx2_le_x3.
-  rewrite -/sorted. 
-  case_eq xs3.
-    by [].
-  move=> x4 xs4.
-  rewrite Bool.andb_true_iff.
-  move=> _.
-  case.
-  by [].
+by [].
 Qed.
 
 Lemma sorted_ind_inv: forall x1 x2 xs3,
-  sorted (x1 :: x2 :: xs3) = true -> sorted (x2 :: xs3) = true /\ x1 <= x2.
+  sorted (x1 :: x2 :: xs3) -> sorted (x2 :: xs3) /\ x1 <= x2.
 Proof.
 move=> x1 x2 xs3 Hsorted.
 split.
 - move: Hsorted.
   rewrite {1}/sorted.
   rewrite -/sorted.
-  (* 次の展開時に -/sorted すること！ *)
-  rewrite Bool.andb_true_iff.
   case.
-    move=> Hx1_le_x2.
-  case xs3.
-    by [].
-  move=> x3 xs4.
-  by rewrite Bool.andb_true_iff.
+  by [].
 - move: Hsorted.
   rewrite /sorted -/sorted.
-  rewrite Bool.andb_true_iff Nat.leb_le.
   by case.
 Qed.
 
-
+Lemma sorted_app: forall xas xa xb xbs,
+  sorted (xas ++ xa :: []) ->
+  sorted (xb :: xbs) ->
+  xa <= xb ->
+  sorted (xas ++ xa :: xb :: xbs).
+Proof.
 
 
 
