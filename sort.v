4c2c2834bd9f14feeaa50e1d1756dfad0fd7d86f
soukouki <soukouki0@yahoo.co.jp>
2022-08-21 14:09:16 +0900

    ソートの先頭が一番小さいことを証明

--- sort.v
+++ sort.v
@@ -39,13 +39,73 @@ split.
   by case.
 Qed.
 
+Lemma sorted_min: forall x1 xs x,
+  sorted (x1 :: xs) ->
+  In x xs ->
+  x1 <= x.
+Proof.
+move=> x1 xs x Hsorted Hinx.
+move: in_split => Hin_split.
+specialize (Hin_split nat x xs).
+move: Hin_split.
+case.
+  by [].
+move=> xs1.
+case.
+move=> xs2 Hxs.
+move: Hsorted.
+rewrite Hxs.
+clear xs Hxs Hinx.
+move: xs1.
+induction xs1.
+- rewrite /=.
+  case.
+  by [].
+- rename a into x2.
+  move=> H.
+  apply IHxs1.
+  move: H.
+  have: x1 :: (x2 :: xs1) ++ x :: xs2 = x1 :: x2 :: xs1 ++ x :: xs2.
+    by [].
+  move=> H.
+  rewrite H.
+  clear H.
+  set xs := xs1 ++ x :: xs2.
+  rewrite /=.
+  case.
+  move=> Hx1_le_x2.
+  case_eq xs => //=.
+  move=> x3 xs3 Hxs.
+  case.
+  move=> Hx2_le_x3 Hxs3.
+  split.
+  + move: Hx1_le_x2 Hx2_le_x3.
+    by apply Nat.le_trans.
+  + by apply Hxs3.
+Qed.
+
 Lemma sorted_app: forall xas xa xb xbs,
   sorted (xas ++ xa :: []) ->
   sorted (xb :: xbs) ->
   xa <= xb ->
   sorted (xas ++ xa :: xb :: xbs).
 Proof.
-
+move=> xas xa xb xbs Hsorted_a Hsorted_b Hxa_le_xb.
+have: sorted (xa :: xb :: xbs).
+  by [].
+move: xas xa Hxa_le_xb Hsorted_a.
+induction xas.
+- by [].
+- rename a into x.
+  move=> xa Hxa_le_xb Hsorted_x_xas_xa Hsorted_xa_xb_xbs.
+  rewrite /sorted /= -/sorted.
+  suff: sorted (xas ++ xa :: xb ::xbs).
+    case xas.
+    - rewrite /=.
+      suff: x <= xa.
+        by [].
+      move: Hsorted_x_xas_xa.
+      
 
 
 
