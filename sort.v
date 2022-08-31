ac88aba7696b9dbf87c64171450ed92c8dd29f08
soukouki <soukouki0@yahoo.co.jp>
2022-08-31 16:45:51 +0900

    なんかエグいことになってるよぉ・・・

--- sort.v
+++ sort.v
@@ -15,6 +15,10 @@ Fixpoint sorted (xs: list nat): Prop :=
   | x1 :: xs1 => (forall x, In x xs1 -> x1 <= x) /\ sorted xs1
   end.
 
+Lemma sorted_ind: forall xs x1,
+  (forall x, In x xs -> x1 <= x) /\ sorted xs -> sorted (x1 :: xs).
+Proof. auto. Qed.
+
 (* Fixpoint sorted_simple (xs: list nat): Prop :=
   match xs with
   | [] => True
@@ -43,37 +47,44 @@ split.
 split=> //.
 Admitted.
 
+Lemma filter_length {A: Type} : forall (xs: list A) f,
+  length (filter f xs) <= length xs.
+Proof.
+move=> xs f.
+induction xs => //.
+rewrite /=.
+case (f a).
+- suff: S (length (filter f xs)) <= S (length xs).
+    by rewrite /=.
+  by rewrite -Nat.succ_le_mono.
+- by apply Nat.le_le_succ_r.
+Qed.
+
 Function quick_sort (xs: list nat) {measure length}: list nat :=
   match xs with
   | [] => []
   | x1 :: [] => [x1]
   | pivot :: xs1 =>
-    let (left, right) := partition (Nat.leb pivot) xs1 in
+    let right := filter (fun x => x <? pivot) xs1 in
+    let left := filter (fun x => pivot <=? x) xs1 in
       (quick_sort right) ++ pivot :: (quick_sort left)
   end.
 Proof.
 (* xs = pivot :: x2 :: xs3 *)
 (* xs1 = x2 :: xs3 *)
-move=> xs pivot xs1 x2 xs2 Hxs1 Hxs left right Hpartition.
-move: (partition_length (Nat.leb pivot) xs1).
-rewrite Hxs1.
-move=> Hlength_xs1.
-specialize (Hlength_xs1 left right Hpartition).
-apply (Nat.le_lt_trans (length left) (length (x2 :: xs2)) (length (pivot :: x2 :: xs2))).
-- rewrite Hlength_xs1.
-  apply Nat.le_add_r.
-- by [].
+move=> xs pivot xs1 x2 xs2 Hxs1 Hxs.
+suff: length (filter _ (x2 :: xs2)) < S (length (x2 :: xs2)).
+  by apply.
+move=> b.
+apply Nat.lt_succ_r.
+by apply filter_length.
 
-(* leftとrightを入れ替えてもう一度同じことをする *)
-move=> xs pivot xs1 x2 xs2 Hxs1 Hxs left right Hpartition.
-move: (partition_length (Nat.leb pivot) xs1).
-rewrite Hxs1.
-move=> Hlength_xs1.
-specialize (Hlength_xs1 left right Hpartition).
-apply (Nat.le_lt_trans (length right) (length (x2 :: xs2)) (length (pivot :: x2 :: xs2))).
-- rewrite Hlength_xs1 Nat.add_comm.
-  apply Nat.le_add_r.
-- by [].
+move=> xs pivot xs1 x2 xs2 Hxs1 Hxs.
+suff: length (filter _ (x2 :: xs2)) < S (length (x2 :: xs2)).
+  by apply.
+move=> b.
+apply Nat.lt_succ_r.
+by apply filter_length.
 Qed.
 
 Example quick_sort_example:
@@ -83,8 +94,6 @@ rewrite quick_sort_equation /=.
 rewrite 2!quick_sort_equation /=.
 by rewrite 2!quick_sort_equation /=.
 Qed.
-
-
 
 Lemma quick_sort_nil:
   quick_sort [] = [].
@@ -106,7 +115,8 @@ move=> x1 x2.
 case_eq (x1 <=? x2).
 + move=> Hx1_le_x2.
   rewrite quick_sort_equation /=.
-  rewrite Hx1_le_x2.
+  rewrite Nat.ltb_antisym.
+  rewrite Hx1_le_x2 /=.
   rewrite quick_sort_nil quick_sort_one /=.
   split=> //=.
   move=> x.
@@ -117,7 +127,8 @@ case_eq (x1 <=? x2).
 + move=> Hx1_nle_x2.
   rewrite /=.
   rewrite quick_sort_equation /=.
-  rewrite Hx1_nle_x2.
+  rewrite Nat.ltb_antisym.
+  rewrite Hx1_nle_x2 /=.
   rewrite quick_sort_one quick_sort_nil /=.
   split=> //=.
   move=> x.
@@ -128,9 +139,16 @@ case_eq (x1 <=? x2).
   by apply Nat.lt_le_incl.
 Qed.
 
+Lemma quick_sort_In: forall xs x,
+  In x xs = In x (quick_sort xs).
+Admitted.
+
+
+
 Theorem quick_sort_sorted: forall xs,
   sorted (quick_sort xs).
-(* 帰納法のbase case *)
+Proof.
+(* 帰納法のbase case...と思ってたけれど、これcaseで十分説がある *)
 move=> xs.
 induction xs.
   by rewrite quick_sort_nil.
@@ -146,7 +164,43 @@ rename a into x3.
 move: IHxs => IHxs3.
 (* 帰納法のinduction step *)
 rewrite quick_sort_equation.
-Search _ partition.
+remember (filter (fun x : nat => x <? x1) (x2 :: x3 :: xs)) as left.
+remember (filter (fun x : nat => x1 <=? x) (x2 :: x3 :: xs)) as right.
+induction left.
+- rewrite quick_sort_nil /=.
+  split.
+    move=> x.
+    rewrite -quick_sort_In.
+    rewrite Heqright.
+    rewrite filter_In.
+    case.
+    move=> _.
+    by apply Compare_dec.leb_complete.
+  induction right.
+    by rewrite quick_sort_nil.
+  rename a into pivot.
+  suff: filter (fun x : nat => x <? pivot) right = [] /\ filter (fun x : nat => pivot <=? x) right = right.
+    case.
+    move=> HfilterR_right HfilterR_left.
+    rewrite quick_sort_equation.
+    rewrite HfilterR_right HfilterR_left.
+    case_eq right => //.
+    move=> x2' xs2' Hright.
+    rewrite quick_sort_nil -Hright /=.
+    clear x2' xs2' Hright.
+    split.
+    + move=> x.
+      rewrite -quick_sort_In.
+      rewrite -HfilterR_left.
+      rewrite filter_In.
+      case.
+      move=> _.
+      apply Nat.leb_le.
+    + apply IHright.
+      
+
+
+
 
 
 
